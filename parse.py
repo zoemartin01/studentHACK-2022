384ac7a1e2838a4f39527684c8146adff5d89594
Zoe Martin <git@zoemartin.me>
2022-11-23 12:11:10 +0100

    add gitignore and update parser

--- parse.py
+++ parse.py
@@ -2,6 +2,10 @@ import csv
 from datetime import datetime
 from typing import List
 
+# GLOBALS
+SKIP_COLUMNS = 3
+FILE_PATH = "./HDE-INT-MOT-20221028-01_HT8_03_N85z-4_OC2_50Hz_70C_R_000759_221107_100743.GEV.csv"
+
 
 class SensorData:
     value: str
@@ -30,46 +34,46 @@ class Sensor:
         return f"Sensor: {self.channel_name}, Tag: {self.tag_no}, Comment: {self.tag_comment}, Unit: {self.unit}"
 
 
-sensors: List[Sensor] = []
+def parse_csv_file(file_path: str) -> List[Sensor]:
+    sensors: List[Sensor] = []
 
-skip_columns = 3
+    print(f"Processing file: {file_path}")
 
+    with open(file_path) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=",")
+        line_count = 0
 
-with open(
-    "./HDE-INT-MOT-20221028-01_HT8_03_N85z-4_OC2_50Hz_70C_R_000759_221107_100743.GEV.csv"
-) as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=",")
-    line_count = 0
+        for row in readCSV:
+            # read channel names
+            if line_count == 26:
+                for idx, name in enumerate(row[SKIP_COLUMNS:]):
+                    sensors.append(Sensor(name))
 
-    for row in readCSV:
-        # read channel names
-        if line_count == 26:
-            for idx, name in enumerate(row[skip_columns:]):
-                sensors.append(Sensor(name))
+            # read tag comments
+            if line_count == 27:
+                for idx, tag_comment in enumerate(row[SKIP_COLUMNS:]):
+                    sensors[idx].tag_comment = tag_comment
 
-        # read tag comments
-        if line_count == 27:
-            for idx, tag_comment in enumerate(row[skip_columns:]):
-                sensors[idx].tag_comment = tag_comment
+            # read tag numbers
+            if line_count == 28:
+                for idx, tag_no in enumerate(row[SKIP_COLUMNS:]):
+                    sensors[idx].tag_no = tag_no
 
-        # read tag numbers
-        if line_count == 28:
-            for idx, tag_no in enumerate(row[skip_columns:]):
-                sensors[idx].tag_no = tag_no
+            # read units
+            if line_count == 29:
+                for idx, unit in enumerate(row[SKIP_COLUMNS:]):
+                    sensors[idx].unit = unit
 
-        # read units
-        if line_count == 29:
-            for idx, unit in enumerate(row[skip_columns:]):
-                sensors[idx].unit = unit
+            # read data
+            if line_count > 29:
+                date = row[0]
+                time = row[1]
 
-        # read data
-        if line_count > 29:
-            date = row[0]
-            time = row[1]
+                for idx, value in enumerate(row[SKIP_COLUMNS:]):
+                    sensors[idx].data.append(SensorData(value.strip(), date, time))
 
-            for idx, value in enumerate(row[skip_columns:]):
-                sensors[idx].data.append(SensorData(value.strip(), date, time))
+            line_count += 1
 
-        line_count += 1
+        print("Processing finished.")
 
-print(sensors)
+    return sensors
