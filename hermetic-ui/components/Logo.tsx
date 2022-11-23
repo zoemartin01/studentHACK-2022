4ffdbaa9eab7e6811b9dcb677966e911d8b1ba1e
Christian Schliz <code@foxat.de>
2022-11-23 22:42:59 +0100

    update logo

--- hermetic-ui/components/Logo.tsx
+++ hermetic-ui/components/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Logo() {
-    return <Image width={140} height={40} src="/logo-hermetic.svg" alt="logo" />
+    return <Image width={140} height={40} src="/hermetic-cloud.svg" alt="logo" />
 }
