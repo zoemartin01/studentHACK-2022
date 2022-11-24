d7aee55ad8622478190a5f76db9f035b81fd98b0
Pablo Schmeiser <upwhd@student.kit.edu>
2022-11-24 01:29:40 +0100

    UI lol

--- hermetic-ui/pages/index.tsx
+++ hermetic-ui/pages/index.tsx
@@ -1,14 +1,31 @@
 import Shell from "../components/AppShell";
-import { MantineProvider } from "@mantine/core";
+import {MantineProvider} from "@mantine/core";
+import {NotificationsProvider, showNotification} from "@mantine/notifications";
+import {useEffect} from "react";
+import Api from "../src/api";
+import {PumpState, pumpStateToColor, pumpStateToString} from "../src/types";
 
 const LocalPage = function () {
+  useEffect(() => {
+    Api.getPumps().filter(value => (value.state < PumpState.Unknown && value.state > PumpState.FullyOperational))
+        .map((pump) => showNotification({
+          title: pumpStateToString(pump.state),
+          color: pumpStateToColor(pump.state),
+          message: 'State of '.concat(pump.name, ' is: ' , pumpStateToString(pump.state)),
+          autoClose: 30000,
+          key: pump.name
+        })
+    )
+  }, [])
   return (
     <MantineProvider
       theme={{ colorScheme: "light" }}
       withGlobalStyles
       withNormalizeCSS
     >
-      <Shell />
+      <NotificationsProvider position={"bottom-right"}>
+        <Shell />
+      </NotificationsProvider>
     </MantineProvider>
   );
 };
